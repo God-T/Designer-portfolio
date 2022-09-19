bf129a5e80d1b79aa7c5e57991776a53d7f772e9
Mysterious Zong <z5092254@gmail.com>
2022-09-20 02:53:05 +1000

    Update webpack.dev.config.js
    - dynamic set entry, HtmlWebpackPlugin

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,14 +1,52 @@
 const path = require('path');
+const glob = require('glob');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 // const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpack = [
+    {
+        entry: 'index',
+        dir: 'src',
+    },
+    {
+        entry: 'project',
+        dir: 'src/html',
+    },
+];
+
+const HtmlWebpackPlugins = HtmlWebpack.map(
+    i =>
+        new HtmlWebpackPlugin({
+            title: i.entry,
+            filename:
+                i.entry === 'index'
+                    ? `${i.entry}.html`
+                    : `${i.entry}/index.html`,
+            template: `${i.dir}/${i.entry}.html`,
+            /* Specify which entry js to load */
+            chunks: [i.entry],
+        })
+);
+const HtmlLoaderExclude = HtmlWebpack.filter(
+    i => !i.dir.includes('common')
+).map(i => path.resolve(__dirname, `${i.dir}/${i.entry}.html`));
+
+const entries = () => {
+    entry = {};
+    HtmlWebpack.map(i => (entry[i.entry] = `./src/js/${i.entry}.js`));
+    return entry;
+};
+/* Alternative way to set entries */
+// const entries = glob.sync('./src/js/*.js').reduce((entries, entry) => {
+//     const entryName = path.parse(entry).name;
+//     entries[entryName] = entry;
+
+//     return entries;
+// }, {});
 
 module.exports = {
-    entry: {
-        index: './src/js/index.js',
-        project: './src/js/project.js',
-    },
+    entry: entries,
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, './dist'),
@@ -104,6 +142,13 @@ module.exports = {
                 // ],
             },
             {
+                test: /\.html$/,
+                exclude: [/(node_modules)/, ...HtmlLoaderExclude],
+                use: {
+                    loader: 'html-loader',
+                },
+            },
+            {
                 test: /\.js$/,
                 exclude: /(node_modules)/,
                 use: {
@@ -113,26 +158,36 @@ module.exports = {
         ],
     },
     plugins: [
+        ...HtmlWebpackPlugins,
         /* Minification of the resulting JS file bundle */
         // new TerserPlugin(),
         /* Extracting CSS into a separate bundle */
         // new MiniCssExtractPlugin({
         //     filename: '[name].min.css',
         // }),
-        new HtmlWebpackPlugin({
-            title: 'index',
-            filename: 'index.html',
-            template: 'src/index.html',
-            /* Specify which entry js to load */
-            chunks: ['index'],
-        }),
-        new HtmlWebpackPlugin({
-            title: 'project',
-            filename: 'project/index.html',
-            template: 'src/html/project.html',
-            /* Specify which entry js to load */
-            chunks: ['project'],
-        }),
+
+        // new HtmlWebpackPlugin({
+        //     title: 'index',
+        //     filename: 'index.html',
+        //     template: 'src/index.html',
+        //     /* Specify which entry js to load */
+        //     chunks: ['index'],
+        // }),
+        // new HtmlWebpackPlugin({
+        //     title: 'project',
+        //     filename: 'project/index.html',
+        //     template: 'src/html/project.html',
+        //     /* Specify which entry js to load */
+        //     chunks: ['project'],
+        // }),
+        // new HtmlWebpackPlugin({
+        //     title: 'placeHolder',
+        //     filename: 'placeHolder/index.html',
+        //     template: 'src/html/common/placeHolder.html',
+        //     /* Specify which entry js to load */
+        //     chunks: ['placeHolder'],
+        // }),
+
         /* Clean build folder(defined in output.path) before generating new bundles */
         // new CleanWebpackPlugin({
         //     cleanOnceBeforeBuildPatterns: [
